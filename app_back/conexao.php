27c3ae473db2b5cfe733e4a6ad72675f2df90fdf
WilliamAndreoli <willandreoli33@gmail.com>
2024-05-09 16:16:48 -0300

    First commit

---
+++ app_back/conexao.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Conexao {
+
+    private $host = 'localhost';
+    private $dbname = 'bd_lista_tarefas';
+    private $user = 'root';
+    private $password = '';
+
+    public function conectar() {
+        try {
+
+            $conexao = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname",
+                "$this->user",
+                "$this->password"
+            );
+
+            return $conexao;
+
+        } catch (PDOException $e) {
+            echo '<p>'.$e->getMessage().'</p>';
+        }
+
+    }
+
+}
+
+?>
